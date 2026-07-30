b41d8acec5b8cb80b94f351d3ce5362f9b201579
mitalipawar2006 <mitalirpawar10@gmail.com>
2026-07-30 19:58:52 +0530

    Convert the first letter of every word to uppercase.py

--- Convert the first letter of every word to uppercase.py
+++ Convert the first letter of every word to uppercase.py
@@ -1,1 +1,5 @@
 # Convert the first letter of every word to uppercase. 
+sentence=str(input("Enter a string:"))
+words=sentence.split()       
+for word in words:
+    print(word.capitalize(),end=" ")
